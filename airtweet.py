80b70fc8e1566800375594c68500dd949db8ca25
Kobnar <konrad.rk.ludwig@gmail.com>
2016-04-20 11:00:12 -0700

    Remove silly global variabel for default ini path

--- airtweet.py
+++ airtweet.py
@@ -5,19 +5,10 @@ import twitter
 import azure
 
 
-_DEFAULT_CONFIG_PATH = 'airtweet.ini'
-
-
-def _read_config_file(config_path):
-    config = configparser.ConfigParser()
-    config.read(config_path)
-    return config
-
-
 class AirTweet(object):
-    def __init__(self, config_path=None):
-        config_path = config_path or _DEFAULT_CONFIG_PATH
-        config = _read_config_file(config_path)
+    def __init__(self, config_path='airtweet.ini'):
+        config = configparser.ConfigParser()
+        config.read(config_path)
 
         # Twitter API
         twitter_config = config['TwitterAPI']
